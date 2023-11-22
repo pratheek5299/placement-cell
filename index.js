e715183a48618606da450b86c88124b6836e1f7a
Pratheek <saipratheekreddykasarla@gmail.com>
2023-11-22 17:30:26 +0530

    Added header footer and scss middleware and some styles

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
 
+const sassMiddleware = require('node-sass-middleware');
+
+app.use(sassMiddleware({
+    src: './assests/scss',
+    dest: './assests/css',
+    debug: true,
+    outputStyle: 'expanded',
+    prefix: '/css'
+}))
+app.use(express.static('./assests'));
 app.use(expressLayouts);
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
